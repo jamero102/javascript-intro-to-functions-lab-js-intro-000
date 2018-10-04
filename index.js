58cf2cc69284ba745b94fe05f2dc621c9e4dd4e2
Jane Amero <janie.amero@gmail.com>
2018-10-04 15:54:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string) {
   return "I can't hear you!"
 } else if (string = uppercase){
   return "YES INDEED"
-} else if (string = 'I love you'){
+} else if (string = 'I love you, Grandma'){
   return "I love you, too"
 }
 }
